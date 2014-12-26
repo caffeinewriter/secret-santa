46ce2e7ce6805eae7d5183c7a5a993645b403742
Brandon Anzaldi <brandon@brandonanzaldi.com>
2014-12-26 05:09:31 -0800

    Adding some mobile sexiness

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,4 +8,21 @@ $(document).ready(function() {
       $('.error').slideUp(400)
     }, 5000);
   }
+  $('.menu-tab .down').click(function() {
+    $('.menu-nav').slideDown(400);
+    $('.menu-tab .up').show();
+    $('.menu-tab .down').hide();
+    var getTop = ($('ul.nav li').length * 4) + 'em';
+    $('.menu-tab').animate({
+      top: getTop
+    },400);
+  });
+  $('.menu-tab .up').click(function() {
+    $('.menu-nav').slideUp(400);
+    $('.menu-tab .down').show();
+    $('.menu-tab .up').hide();
+    $('.menu-tab').animate({
+      top: 0
+    },400);
+  });
 });
