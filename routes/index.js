fd8d0693121de26e101afe37a5d5151ebe276528
Brandon Anzaldi <brandon@brandonanzaldi.com>
2015-12-03 22:45:08 -0800

    Fixing spelling mistake and confirmation

--- routes/index.js
+++ routes/index.js
@@ -192,8 +192,8 @@ router.post('/signup', isAuthenticated, function (req, res) {
       user.uid = invite.uid;
       user.password = sanitizer.escape(req.body.password);
       user.interests = sanitizer.escape(req.body.interests);
-      if (!!req.body.address1 && req.body.address1.length > 0) {
-        user.address = sanitizer.escape(req.body.address1 + "\n" + (req.body.address2.length > 0 ? req.body.address2 + "\n" : '') + req.body.city + ', ' + (req.body.state ? req.body.state + ', ' : '') + req.body.postal + ', ' + req.body.country);
+      if (!!req.body.address1 && req.body.address1.length > 0 && !!req.body.city && req.body.city.length > 0 && !!req.body.postal && req.body.postal > 0 && !!req.body.country && req.body.country.length > 0) {
+        user.address = sanitizer.escape(req.body.address1 + "\n" + (!!req.body.address2 && req.body.address2.length > 0 ? req.body.address2 + "\n" : '') + req.body.city + ', ' + (req.body.state ? req.body.state + ', ' : '') + req.body.postal + ', ' + req.body.country);
       }
       invite.claimed = true;
       invite.save(function (err) {
